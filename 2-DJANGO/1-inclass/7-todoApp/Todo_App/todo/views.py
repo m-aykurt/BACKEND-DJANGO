54279003ff069bddf519885c04619c9d2e4aed77
m_aykurt <maykurt92@gmail.com>
2021-10-23 15:08:34 +0300

    AUTH-1

---
+++ 2-DJANGO/1-inclass/7-todoApp/Todo_App/todo/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Todo
+from .forms import TodoAddForm, TodoUpdateForm
+
+
+def home(request):
+    return render(request, "todo/home.html")
+
+
+def todo_list(request):
+    todos = Todo.objects.all()
+    form = TodoAddForm()
+
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = {
+        "todos": todos,
+        "form": form,
+    }
+    return render(request, "todo/todo_list.html", context)
+
+
+def todo_add(request):
+    form = TodoAddForm()
+
+    if request.method == "POST":
+        form = TodoAddForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "todo/todo_add.html", context)
+
+
+def todo_update(request, id):
+    # todo = Todo.objects.get(id=id)
+    todo = get_object_or_404(Todo, id=id)
+    form = TodoUpdateForm(instance=todo)
+
+    if request.method == "POST":
+        form = TodoUpdateForm(request.POST, instance=todo)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = {
+        "form": form,
+        "todo": todo
+    }
+    return render(request, "todo/todo_update.html", context)
+
+
+def todo_delete(request, id):
+    todo = get_object_or_404(Todo, id=id)
+    if request.method == "POST":
+        todo.delete()
+        return redirect("list")
+
+    context = {
+        "todo": todo,
+    }
+    return render(request, "todo/todo_delete.html", context)
