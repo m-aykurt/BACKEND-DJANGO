7f88fc26afe670fb1656d93afeab858fedbf5fbc
m_aykurt <maykurt92@gmail.com>
2021-10-25 14:51:56 +0300

    auth-last commit

---
+++ 2-DJANGO/1-inclass/9-AUTH/Django-Auth-2/users/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+from django.contrib import messages
+
+from .forms import UserForm
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'users/home.html')
+
+
+def register(request):
+    form = UserForm()
+    if request.method == 'POST':
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+
+    context = {
+        'form_user': form
+    }
+    return render(request, "users/register.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    messages.success(request, "You Logout!")
+    return redirect('home')
+
+
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+        user = form.get_user()
+        if user:
+            messages.success(request, "Login successfull")
+            login(request, user)
+            return redirect('home')
+    return render(request, 'users/user_login.html', {"form": form})
---
+++ 2-DJANGO/1-inclass/9-AUTH/Django-Auth-3/users/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+# Create your models here.
+
+
+# class User(AbstractUser):
+#     portfolio = models.URLField(blank=True)
+#     profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("Email field is mandatory")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_super_user(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("is_super_user", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_stuff True")
+        if extra_fields.get("is_active") is not True:
+            raise ValueError("Superuser must have is_active True")
+        if extra_fields.get("is_super_user") is not True:
+            raise ValueError("Superuser must have is_super_user True")
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
